56f5c2c5411a3dc331e9c7103ff60c7b045dbba0
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-05 14:21:24 +0100

    Setup admin page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -4,6 +4,11 @@ from .models import Item
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
+
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta"],
+                   "ingredients": ["things"]}
+        return context
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
