b46b4641041164be0282234aa4e397da77ffe2a0
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-06 12:25:45 +0100

    Added an about page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meals": ["Pizza", "Pasta"],
-                   "ingredients": ["things"]}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["mealtypes"] = MEAL_TYPE
         return context
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
+
+class About(generic.ListView):
+    model = Item
+    template_name = "about.html"
